ead6b6831d8ee8912fde3443f8519ccd9fe63343
Jesus Estevez <jesestrod1@gmail.com>
2021-10-15 19:40:55 +0200

    Initial commit

---
+++ src/Providers/InitarServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jestevezrod\Initar\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InitarServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+}
